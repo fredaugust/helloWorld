743fec4c6b4f388eeb58028dfcab588908dc9950
FredAugust <fedster@gmail.com>
2014-03-07 18:33:49 -0800

    *

---
+++ main.cpp
@@ -0,0 +1,33 @@
+
+int doSomething(const double &val)
+{
+    double a = val;
+    return a;
+}
+
+int testPtrs(const double *dp1, double * const dp2, const double &dp3, const int dp4)
+{
+    double b;
+    //*dp1 = 4.0; // fails
+    dp1 = &b;
+    //*dp3 = 4;
+    *dp2 = 3;
+    //dp2 = &b;
+    int r = dp4;
+    //dp4++;
+    doSomething(dp3);
+    return 0;
+}
+
+int testAgain(double const *val)
+{
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
+    double a = 5; double b = 4; double c = 3; int d = 2;
+    testPtrs(&a, &b, c, d);
+    return 0;
+}
---
+++ testConstPtrs.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-02-04T18:07:18
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = testConstPtrs
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
